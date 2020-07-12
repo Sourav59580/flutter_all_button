f2afe7f49d66fb2776d932e381dd3605e0751b52
sourav santra <souravsantra59580@gmail.com>
2020-07-12 21:13:19 +0530

    first commit(12/07/2020)

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Flutter layout",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Navbar"),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            // ===========   ButtonBar   ===========
+            ButtonBar(
+              children: <Widget>[
+                RaisedButton(
+                  child: Text("click",style: TextStyle(color: Colors.white),),
+                  color: Colors.red,
+                  onPressed: (){},
+                ),
+               RaisedButton(
+                 child: Text("Submit",style: TextStyle(color: Colors.white),),
+                 color: Colors.black,
+                 onPressed: (){},
+               ),
+                Text("Read More",style: TextStyle(color: Colors.purple),),
+              ],
+            ),
+
+            // ========== FlatButton ============
+            Container(
+              margin: EdgeInsets.all(10),
+              child: FlatButton(
+                child: Text("Button"),
+                onPressed: (){},
+                color: Colors.blue,
+                textColor: Colors.white,
+                splashColor: Colors.yellow,
+              ),
+            ),
+            //============ OutLine Button ===========
+            Container(
+              child: OutlineButton(
+                child: Text("Button"),
+                onPressed: (){},
+                textColor: Colors.purple,
+                borderSide: BorderSide(
+                  color: Colors.black
+                ),
+              ),
+            ),
+            // ========= Icon Button ============
+            Container(
+              child: IconButton(
+                icon: Icon(Icons.favorite,color: Colors.pink,),
+                onPressed: (){},
+              ),
+            ),
+            // ============ Pop Up button ============
+            Container(
+              child: Popupbutton(),
+            ),
+// =============  IconButton =============
+            Container(
+              child: Expanded(
+                child: IconButton(
+                  icon: Icon(Icons.volume_up),
+                  color: Colors.red,
+                  onPressed: (){},
+                ),
+              ),
+            ),
+            // ============= RaiseButton =======
+
+
+
+
+
+
+          ],
+        )
+
+      ),
+
+      // ========== FloatingActionButton  ============
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add,color: Colors.white,),
+        backgroundColor: Colors.red,
+        splashColor: Colors.yellow,
+
+    ),
+
+
+    ),
+  ));
+}
+
+class Popupbutton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: ,
+    );
+  }
+}
